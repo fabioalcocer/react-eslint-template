d76c39f8f4ecbab6ad15fc58adadad6e2a870f55
fabio alcocer <fabioalcocersejas17@gmail.com>
2022-10-17 14:37:38 -0400

    add plugin tailwind classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 function App () {
   return (
-    <div className='bg-slate-800 min-h-screen flex items-center justify-center'>
-      <h1 className='text-3xl font-bold text-teal-500'>Vite + React!</h1>
+    <div className='flex min-h-screen items-center justify-center bg-slate-800'>
+      <h1 className='text-3xl font-bold text-teal-500'>
+        Vite + React!
+      </h1>
     </div>
   )
 }
